edef8aae24b4f40ed5fb089b8d2fad939493a042
Adam Hartman <adamhartman5@gmail.com>
2021-07-31 00:48:30 -0400

    Start building header and allow image domain

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,43 @@
+import Image from "next/image";
+import {
+    BellIcon,
+    ChatIcon,
+    ChevronDownIcon,
+    HomeIcon,
+    UserGroupIcon,
+    ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+    FlagIcon,
+    PlayIcon,
+    SearchIcon,
+    ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
 function Header() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h1>Header</h1>
+
+      {/* Left */}
+      <div>
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+      </div>
+      <div>
+
+          <input type="text" placeholder="Search Facebook" />
+      </div>
+
+      {/* Center */}
+
+      {/* Right */}
+    </div>
+  );
 }
 
-export default Header
+export default Header;
